50e42052cbb9938735d73f7daf36914cfbdffd25
bheemaguli <sumanth.bhi@gmail.com>
2022-09-21 14:05:46 +0530

    Part 3 - Templates

--- web_app/settings.py
+++ web_app/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Additional Apps
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
