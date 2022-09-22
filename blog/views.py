4e0cbff5b2eae6173dda015527f057951906de36
bheemaguli <sumanth.bhi@gmail.com>
2022-09-22 18:14:21 +0530

    Part 5 - Database and Migrations

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Jon Doe',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'September 7, 2022'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'October 3, 2022'
-    }
-]
-
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
