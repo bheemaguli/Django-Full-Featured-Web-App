85e4eebe0a3f1dfe951e491878c604159be6dae7
bheemaguli <sumanth.bhi@gmail.com>
2022-09-30 17:41:13 +0530

    Part 8 - User Profile and Picture

--- users/urls.py
+++ users/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 from django.urls import path
 
@@ -9,3 +11,6 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='user-logout'),
     path('profile/', views.profile, name='user-profile')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
